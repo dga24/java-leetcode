37c1a1aa3e5b24f0c352602801f8625872a0e0e2
dga24 <garciadavid.24@gmail.com>
2022-08-02 09:52:50 +0200

    ok

--- longestPalindrome/longestPalindrome.java
+++ longestPalindrome/longestPalindrome.java
@@ -2,6 +2,12 @@ package longestPalindrome;
 
 import java.util.Arrays;
 
+
+//409. Longest Palindrome
+// Given a string s which consists of lowercase or uppercase letters, 
+//return the length of the longest palindrome that can be built with those letters.
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome here
+
 public class longestPalindrome {
 
     public static void main(String[] args){
@@ -14,7 +20,7 @@ public class longestPalindrome {
 		for (char c : s.toCharArray()) countAbc[c - 'A']++;
 		int odd = 0;
 		for (char c = 'A'; c <= 'z'; c++) odd += (countAbc[c - 'A'] & 1);
-        
+
 		return s.length() - odd + (odd > 0 ? 1 : 0);
 	}
     
