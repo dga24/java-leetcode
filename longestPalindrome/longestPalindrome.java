1e860a7617eddee52717aa3d1f9daca1dec8eadb
dga24 <garciadavid.24@gmail.com>
2022-08-02 09:05:29 +0200

    palindrome ok

--- longestPalindrome/longestPalindrome.java
+++ longestPalindrome/longestPalindrome.java
@@ -1,16 +1,20 @@
 package longestPalindrome;
 
+import java.util.Arrays;
+
 public class longestPalindrome {
 
-    public static void main(){
+    public static void main(String[] args){
         System.out.println(longestPalindrome("kahdfkajsfh"));
     }
 
     public static int longestPalindrome(String s){
-		int[] count = new int['z' - 'A' + 1];
-		for (char c : s.toCharArray()) count[c - 'A']++;
+		int[] countAbc = new int['z' - 'A' + 1];
+        System.out.println(Arrays.toString(countAbc));
+		for (char c : s.toCharArray()) countAbc[c - 'A']++;
 		int odd = 0;
-		for (char c = 'A'; c <= 'z'; c++) odd += (count[c - 'A'] & 1);
+		for (char c = 'A'; c <= 'z'; c++) odd += (countAbc[c - 'A'] & 1);
+        
 		return s.length() - odd + (odd > 0 ? 1 : 0);
 	}
     
